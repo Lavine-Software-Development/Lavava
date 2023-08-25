1aac61a5a6a892594dd7e8d727f70626d98a6100
Ian <ian.lavine@mail.utoronto.ca>
2023-08-24 22:39:05 -0400

    moved drawing to own class

--- client.py
+++ client.py
@@ -1,13 +1,9 @@
 import pygame as p
 import math
 from network import Network
+from draw import Draw
 
 p.init()
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 1000
-size = (SCREEN_WIDTH, SCREEN_HEIGHT)
-screen = p.display.set_mode(size)
-p.display.set_caption("Lavava")
 
 WHITE = (255, 255, 255)
 
@@ -17,72 +13,7 @@ board = n.getBoard()
 nodes = board.nodes
 edges = board.edges
 clock = p.time.Clock()
-
-def size_factor(x):
-    if x<5:
-        return 0
-    if x>=200:
-        return 1
-    return max(min(math.log10(x/10)/2+x/1000+0.15,1),0)
-
-def draw_arrow(edge, color, start, end, triangle_size=5, spacing=9):
-    
-    dx = end[0] - start[0]
-    dy = end[1] - start[1]
-    magnitude = math.sqrt(dx*dx + dy*dy)
-    
-    dx /= magnitude
-    dy /= magnitude
-    
-    num_triangles = int((magnitude-10) / spacing)
-    
-    length_factor = 1.5
-    
-    for i in range(1, num_triangles + 1):
-        pos = (start[0] + i * spacing * dx +5*dx, start[1] + i * spacing * dy+5*dy)
-        
-        point1 = pos
-        point2 = (pos[0] - length_factor * triangle_size * dx + triangle_size * dy, pos[1] - length_factor * triangle_size * dy - triangle_size * dx)
-        point3 = (pos[0] - length_factor * triangle_size * dx - triangle_size * dy, pos[1] - length_factor * triangle_size * dy + triangle_size * dx)
-
-        if edge.flowing:
-            p.draw.lines(screen, color, True, [point1, point2, point3])
-        else:
-            p.draw.polygon(screen, (120,120,120), [point1, point2, point3])
-
-def draw_circle(edge, color, start, end, circle_radius=3, spacing=6):
-    
-    dx = end[0] - start[0]
-    dy = end[1] - start[1]
-    magnitude = math.sqrt(dx*dx + dy*dy)
-    
-    dx /= magnitude
-    dy /= magnitude
-    
-    num_circles = int((magnitude-10) / spacing)
-    
-    for i in range(1, num_circles + 1):
-        pos = (start[0] + i * spacing * dx+5*dx, start[1] + i * spacing * dy+5*dy)
-        if edge.flowing:
-            p.draw.circle(screen, color, (int(pos[0]), int(pos[1])), circle_radius, 1)
-        else:
-            p.draw.circle(screen, (120,120,120), (int(pos[0]), int(pos[1])), circle_radius)
-            
-def blit_edges():
-    for edge in edges:
-        if edge.directed:
-            draw_arrow(edge,(50,50,50),edge.from_node.pos,edge.to_node.pos)         
-        else:
-            draw_circle(edge,(50,50,50),edge.from_node.pos,edge.to_node.pos)
-
-def blit_nodes():
-    for spot in nodes:
-        p.draw.circle(screen, spot.color, spot.pos, int(5+size_factor(spot.value)*18))
-
-font = p.font.Font(None, 60)
-def blit_score():
-    p.draw.rect(screen,WHITE,(0,0,SCREEN_WIDTH,SCREEN_HEIGHT/13))
-    screen.blit(font.render(str(int(player.score)),True,player.color),(20,20))
+d = Draw(edges, nodes, player)
 
 running = True
 shitcount = 0
@@ -94,11 +25,7 @@ while running:
         if event.type == p.QUIT:
             running = False
 
-    screen.fill(WHITE)
-    blit_edges()
-    blit_nodes()
-    blit_score()
-    p.display.update()    
+    d.blit()
     clock.tick()
     shitcount+=1
     if shitcount %10==0:
