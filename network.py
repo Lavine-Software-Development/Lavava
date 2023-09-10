f336e18397a5edafc2263d331af50ad85449f037
Ian <ian.lavine@mail.utoronto.ca>
2023-09-10 12:35:10 -0400

    server controls multiple games. Choose player count and enter specific game

--- network.py
+++ network.py
@@ -4,26 +4,61 @@ from helpers import unwrap_board
 from constants import *
 
 class Network:
-
     def __init__(self, action_callback, tick_callback):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server = NETWORK
         self.port = 5555
         self.addr = (self.server, self.port)
-        self.player, self.board = self.connect()
-        self.action_callback = action_callback
-        self.tick_callback = tick_callback 
+        
+        # Get user input for hosting or joining a game
+        while True:
+            while True:
+                user_input = input("Do you want to HOST or JOIN a game? (HOST/JOIN): ").strip().upper()
+                if user_input in ["HOST", "JOIN"]:
+                    break
+                else:
+                    print("Invalid input. Please enter 'HOST' or 'JOIN'.")
+            
+            if user_input == "HOST":
+                player_count = input("Enter the number of players for the game: ")
+                self.init_data = f"HOST,{player_count}"
+            else:
+                game_code = input("Enter the game code to join: ")
+                self.init_data = f"JOIN,{game_code}"
+            
+            if vals := self.connect():
+                self.player, self.board = vals
+                self.action_callback = action_callback
+                self.tick_callback = tick_callback 
 
-        threading.Thread(target=self.listen_for_data).start()
+                threading.Thread(target=self.listen_for_data).start()
+                break
 
     def connect(self):
         try:
             self.client.connect(self.addr)
-            data = self.client.recv(100*1024*1024)
-            print(data)
-            return unwrap_board(data.decode())
+            
+            # Send initial data for hosting/joining the game
+            self.client.send(self.init_data.encode())
+
+            data = self.client.recv(1024)  # First receive success or failure message
+            response = data.decode()
+            
+            # Check if the response is a failure message
+            if response == "INVALID_CODE":
+                print("Failed to join the game: Invalid game code.")
+                return False
+            else:
+                print("Connected to game", response)
+                
+                # Wait for the board data
+                data = self.client.recv(100*1024*1024)
+                return unwrap_board(data.decode())  # Now we expect the board data
+
         except:
-            return "Connection failed"
+            print("Connection failed.")
+            return False
+
 
     def send(self, data):
         try:
@@ -31,7 +66,6 @@ class Network:
             self.client.send(message.encode())
         except socket.error as e:
             print(e)
-
 
     def listen_for_data(self):
         while True:
--- server.py
+++ server.py
@@ -6,13 +6,16 @@ import sys
 import time
 from threading import Thread
 from constants import *
+import random
+import string
 
 class Server:
-    def __init__(self, ip, port):
+    def __init__(self, port):
         self.server = '0.0.0.0'
         self.port = port
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.waiting_players = Queue()
+        self.waiting_players = {}  # Stores the waiting players with the game code as the key
+        self.games = {}  # Stores the active games with the game code as the key
 
         try:
             self.s.bind((self.server, self.port))
@@ -23,8 +26,12 @@ class Server:
         self.s.listen(10)
         print("Waiting for a connection, Server Started")
 
+    def generate_game_code(self):
+        """Generate a unique game code."""
+        return ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+
     def send_ticks(self, game):
-        time.sleep(1)  # Wait for 1 second before starting
+        time.sleep(1)
         tick_message = "0,0,0,"
         while True:
             for i, connection in enumerate(game.connections):
@@ -32,16 +39,44 @@ class Server:
                     connection.sendall(tick_message.encode())
                 except OSError as e:
                     print(f"Error on connection {i}: {e}")
-                    # Remove the bad connection if needed
                     del game.connections[i]
             time.sleep(0.1)
 
+    def threaded_client(self, conn):
+        data = conn.recv(1024).decode()
+        is_host, player_count_or_code = data.split(",")
+        print(is_host, player_count_or_code)
 
-    def threaded_client(self, player, game):
-        game.connections[player].send(game.graph.repr(player).encode())
+        if is_host == "HOST":
+            player_count = int(player_count_or_code)
+            game_code = self.generate_game_code() # Generate a unique game code for the new game
+            self.waiting_players[game_code] = {"player_count": player_count, "players": [conn]}
+            conn.sendall(game_code.encode())
+        elif is_host == "JOIN":
+            if player_count_or_code in self.waiting_players:
+                conn.sendall(player_count_or_code.encode())
+                self.waiting_players[player_count_or_code]["players"].append(conn)
+                
+                if len(self.waiting_players[player_count_or_code]["players"]) == self.waiting_players[player_count_or_code]["player_count"]:
+                    game = Game()
+                    game.connections = self.waiting_players.pop(player_count_or_code)["players"]
+                    self.start_game(game, player_count_or_code)
+            else:
+                conn.sendall("INVALID_CODE".encode())
+
+    def start_game(self, game, game_code):
+        tick_thread = Thread(target=self.send_ticks, args=(game,))
+        tick_thread.daemon = True
+        tick_thread.start()
+
+        for i, conn in enumerate(game.connections):
+            start_new_thread(self.threaded_client_in_game, (i, conn, game))
+
+    def threaded_client_in_game(self, player, conn, game):
+        conn.send(game.graph.repr(player).encode())
         while True:
             try:
-                data = game.connections[player].recv(32)
+                data = conn.recv(32)
                 if not data:
                     print("Disconnected")
                     break
@@ -51,28 +86,17 @@ class Server:
                         connection.sendall(data)
             except socket.error as e:
                 print(e)
-
+        
         print("Lost connection")
-        game.connections[player].close()
+        conn.close()
 
     def run(self):
         while True:
             conn, addr = self.s.accept()
             print("Connected to:", addr)
             
-            self.waiting_players.put(conn)
-            if self.waiting_players.qsize() == PLAYER_COUNT:
-                game = Game()
-                for i in range(PLAYER_COUNT):
-                    game.add_player(self.waiting_players.get())
-
-                tick_thread = Thread(target=self.send_ticks, args=(game,))
-                tick_thread.daemon = True
-                tick_thread.start()
-
-                for i in range(PLAYER_COUNT):
-                    start_new_thread(self.threaded_client, (i, game))
+            start_new_thread(self.threaded_client, (conn,))
 
 if __name__ == "__main__":
-    server = Server(NETWORK, 5555)
+    server = Server(5555)
     server.run()
