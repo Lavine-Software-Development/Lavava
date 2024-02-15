b321bb8c54a946d13ef61d05c0d70cff3b1a4636
Ian <ian.lavine@mail.utoronto.ca>
2024-02-15 17:16:58 -0500

    MODES

--- modeConstants.py
+++ modeConstants.py
@@ -23,10 +23,19 @@ class MKD(dict): # Multi Key Dictionary
         for key, value in kwargs.items():
             self.__setitem__(key, value)
 
+PLAYER = None
+MANAGER = None
+START_NODE_ALGORITHM = None
+START_NODE_STATE_ALGORITHM = None
+CAPITAL_TYPE = None
+ABILITY_DISPLAY = None
+ABILITY_OPTION = None
+
 MODE_PLAYERS = MKD(((1, 3), MoneyPlayer), (2, DefaultPlayer))
 MODE_ABILITY_MANAGERS = MKD(((1, 3), MoneyAbilityManager), (2, ReloadAbilityManager))
 STARTING_NODES = MKD(((1, 2), starter_default_nodes), (3, starter_port_nodes))
 STARTING_NODES_STATES = MKD(((1, 3), starter_mines), (2, starter_capitals))
 CAPITAL_TYPES = MKD(((1, 3), CapitalState), (2, StartingCapitalState))
-ABILITY_DISPLAY = MKD(((1, 3), "costs"), (2, "total"))
+ABILITY_DISPLAYS = MKD(((1, 3), "costs"), (2, "total"))
 ABILITY_OPTIONS = MKD((1, m1_ability_options), (2, m2_ability_options), (3, ALL_ABILITIES))
+
