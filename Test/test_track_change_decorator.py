ec436d75d115c5d302cc24f5d64ec29d0253bb1d
Ian <ian.lavine@mail.utoronto.ca>
2024-04-19 23:03:18 -0400

    tcd can do tuples

--- Test/test_track_change_decorator.py
+++ Test/test_track_change_decorator.py
@@ -1,6 +1,6 @@
 import pytest
 
-def track_changes(*attribute_names):
+def track_changes(*attributes):
     def class_decorator(cls):
         original_init = cls.__init__
 
@@ -10,25 +10,30 @@ def track_changes(*attribute_names):
                 return getattr(self, storage_name)
             return getter
 
-        def make_setter(attribute_name):
+        def make_setter(attribute_name, attribute_to_track):
             storage_name = '_' + attribute_name
             def setter(self, value):
-                self.tracked_attributes.add(attribute_name)
+                self.tracked_attributes.add(attribute_to_track)
                 setattr(self, storage_name, value)
             return setter
 
-        for attribute_name in attribute_names:
-
-            # Setting the property on the class
-            getter_func = make_getter(attribute_name)
-            setter_func = make_setter(attribute_name)
-            setattr(cls, attribute_name, property(getter_func, setter_func))
+        for attribute in attributes:
+            if isinstance(attribute, tuple):
+                attribute, attribute_to_track = attribute
+                getter_func = make_getter(attribute)
+                setter_func = make_setter(attribute, attribute_to_track)
+            else:
+                getter_func = make_getter(attribute)
+                setter_func = make_setter(attribute, attribute)
+            setattr(cls, attribute, property(getter_func, setter_func))
 
         # Modify the __init__ to initialize properties
         def new_init(self, *args, **kwargs):
             self.tracked_attributes = set()
 
-            for attribute_name in attribute_names:
+            for attribute_name in attributes:
+                if isinstance(attribute_name, tuple):
+                    attribute_name = attribute_name[0]
                 storage_name = '_' + attribute_name
                 setattr(self, storage_name, None)
 
@@ -49,18 +54,20 @@ class ReferenceClass:
         self.var = 0
 
 @track_changes("base_variable", "reference")
-class BaseClass(JsonableTracked):
+class BaseClass():
     def __init__(self, reference: ReferenceClass):
         self.base_variable = 0
         self.reference = reference
         self.untracked = "hello"
 
-@track_changes("child_variable")
+@track_changes(("child_variable", "tracked_variable"))
 class ChildClass(BaseClass):
     def __init__(self, reference: ReferenceClass):
         super().__init__(reference)
         self.child_variable = 0
-        self.bean = "beani"
+    
+    def tracked_variable(self):
+        return self.child_variable * 2
 
 def test_base_class():
     ref = ReferenceClass()
@@ -82,7 +89,7 @@ def test_child_class():
 
     child.base_variable = 5
     child.child_variable = 5
-    assert child.tracked_attributes == {"base_variable", "child_variable"}
+    assert child.tracked_attributes == {"base_variable", "tracked_variable"}
 
     child.clear()
     assert child.tracked_attributes == set()
