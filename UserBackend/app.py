bdbee919882532602985b6a334d7cf7c424c6090
Akash Ilangovan <akash.ilangovan30@gmail.com>
2024-07-23 01:08:07 -0700

    Testing CI/CD pipeline

--- UserBackend/app.py
+++ UserBackend/app.py
@@ -675,5 +675,5 @@ def get_user_details(username):
 
 
 if __name__ == '__main__':
-    app.run( debug=True, host='0.0.0.0', port=5001)
+    app.run( debug=True, host='0.0.0.0', port=5001,ssl_context=('fullchain.pem', 'privkey.pem'))
     
