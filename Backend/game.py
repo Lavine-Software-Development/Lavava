d08a035bb019f2b68c41d16d4719038b2592a931
Ian <ian.lavine@mail.utoronto.ca>
2024-08-22 01:20:16 -0400

    dumb mistake

--- Backend/game.py
+++ Backend/game.py
@@ -5,52 +5,61 @@ from gameStateEnums import GameStateEnum as GSE
 from board import Board
 from map_builder import MapBuilder
 from ae_effects import make_ability_effects
-from player import CreditPlayer, RoyalePlayer
+from player import CreditPlayer, BasicPlayer
 from ae_effects import make_event_effects
 from ae_validators import make_effect_validators
 from event import Event
 import sys
 from baseAI import create_ai, AI
 
+
 class ServerGame(JsonableTick):
     def __init__(self, player_count, gs, settings):
-
         self.running = True
         self.gs = gs
         self.settings = settings
         self.extra_info = []
         self.counts = [0] * player_count
-        self.board = Board(self.gs, settings['ability_type'] == "credits")
+        self.board = Board(self.gs, settings["ability_type"] == "credits")
         if settings["ability_type"] == "credits":
-            self.player_dict = {
-                i: CreditPlayer(i) for i in range(player_count)
-            }
+            self.player_dict = {i: CreditPlayer(i) for i in range(player_count)}
         else:
             self.player_dict = {
-                i: RoyalePlayer(i, settings) for i in range(player_count)
+                i: BasicPlayer(i, settings) for i in range(player_count)
             }
 
-        start_values = {'board'}
-        tick_values = {'countdown_timer', 'gs', 'extra_info', 'counts'}
-        recurse_values = {'board'}
-        super().__init__('game', start_values, recurse_values, tick_values)
+        start_values = {"board"}
+        tick_values = {"countdown_timer", "gs", "extra_info", "counts"}
+        recurse_values = {"board"}
+        super().__init__("game", start_values, recurse_values, tick_values)
 
         self.restart()
 
     @property
     def countdown_timer(self):
         return self.times[self.current_section]
-    
+
     def get_ordered_counts(self):
         return sorted(self.counts, reverse=True)
-    
+
     def create_bot(self, player_id, ability_process):
-        self.player_dict[player_id] = create_ai(player_id, self.settings, self.board, self.effect, self.event, self.get_ordered_counts, self.eliminate, ability_process)
+        self.player_dict[player_id] = create_ai(
+            player_id,
+            self.settings,
+            self.board,
+            self.effect,
+            self.event,
+            self.get_ordered_counts,
+            self.eliminate,
+            ability_process,
+        )
         return self.player_dict[player_id].name
 
     @property
     def bots(self):
-        return [player for player in self.player_dict.values() if isinstance(player, AI)]
+        return [
+            player for player in self.player_dict.values() if isinstance(player, AI)
+        ]
 
     def effect(self, key, player_id, data):
         player = self.player_dict[player_id]
@@ -59,7 +68,7 @@ class ServerGame(JsonableTick):
                 data_items = [self.board.id_dict[d] for d in data]
                 if data_items[0].owner is None:
                     self.ability_effects[key](data_items, player)
-                    player.ps.next()       
+                    player.ps.next()
                     return True
         else:
             try:
@@ -68,13 +77,17 @@ class ServerGame(JsonableTick):
             except KeyError:
                 print("failed to use ability because item no longer exists")
         return False
-        
+
     def event(self, key, player_id, data):
         player = self.player_dict[player_id]
         event = self.events[key]
         if event.can_use(player, data):
             # the following code is GROSS
-            if len(data) == 3 and data[2] in player.abilities and player.abilities[data[2]].can_afford:
+            if (
+                len(data) == 3
+                and data[2] in player.abilities
+                and player.abilities[data[2]].can_afford
+            ):
                 # thus they used a cannon ability shot
                 self.update_extra_info(("cannon_shot", (data[0], data[1], data[2])))
                 player.abilities[data[2]].use([self.board.id_dict[data[1]]])
@@ -84,7 +97,7 @@ class ServerGame(JsonableTick):
         else:
             print("failed to use event")
             return False
-    
+
     def eliminate(self, player, forced=False):
         rank = len(self.remaining)
         self.remaining.remove(player)
@@ -98,14 +111,21 @@ class ServerGame(JsonableTick):
         self.extra_info.append(data)
 
     def restart(self):
-
-        self.accessibility_times = self.settings['accessibility_times'] if self.settings['iterative_make_accessible'] else []
+        self.accessibility_times = (
+            self.settings["accessibility_times"]
+            if self.settings["iterative_make_accessible"]
+            else []
+        )
 
         for player in self.player_dict.values():
             player.default_values()
         self.remaining = {i for i in range(len(self.player_dict))}
 
-        self.times = [COUNTDOWN_LENGTH, self.settings["main_time"], self.settings["overtime"]]
+        self.times = [
+            COUNTDOWN_LENGTH,
+            self.settings["main_time"],
+            self.settings["overtime"],
+        ]
         self.current_section = 0
 
         map_builder = MapBuilder()
@@ -115,43 +135,59 @@ class ServerGame(JsonableTick):
         self.ability_effects = make_ability_effects(self.board, self.settings)
         self.events = self.make_events_dict()
 
-        
     def make_events_dict(self):
         validators = make_effect_validators(self.board)
-        effects = make_event_effects(self.board, self.update_extra_info, self.settings["cannon_shot_decrease"])
+        effects = make_event_effects(
+            self.board, self.update_extra_info, self.settings["cannon_shot_decrease"]
+        )
         return {code: Event(validators[code], effects[code]) for code in EVENTS}
 
     def set_abilities(self, player, abilities, settings):
         print("setting abilities for player", player, "with abilities", abilities)
-        self.player_dict[player].set_abilities(abilities, self.ability_effects, self.board, settings)
+        self.player_dict[player].set_abilities(
+            abilities, self.ability_effects, self.board, settings
+        )
 
     def set_player_settings(self, player, settings):
-        self.player_dict[player].set_settings(settings) 
+        self.player_dict[player].set_settings(settings)
 
     def all_player_next(self):
         for player in self.player_dict.values():
             player.ps.next()
-    
+
     @property
     def all_player_starts_selected(self):
-        return all([p.ps.state in (PSE.START_WAITING, PSE.ELIMINATED) for p in self.player_dict.values()])
-    
-    @property 
-    def no_player_starts_selected(self):
-        return all([p.ps.state == PSE.START_SELECTION for p in self.player_dict.values()]) 
-    
-    def update_timer(self):
+        return all(
+            [
+                p.ps.state in (PSE.START_WAITING, PSE.ELIMINATED)
+                for p in self.player_dict.values()
+            ]
+        )
 
+    @property
+    def no_player_starts_selected(self):
+        return all(
+            [p.ps.state == PSE.START_SELECTION for p in self.player_dict.values()]
+        )
+
+    def update_timer(self):
         if self.countdown_timer > 0:
             self.times[self.current_section] -= 0.1
 
             if self.accessibility_times and self.gs.value == GSE.PLAY.value:
-                if self.times[self.current_section] <= self.settings['main_time'] - self.accessibility_times[0]:  
+                if (
+                    self.times[self.current_section]
+                    <= self.settings["main_time"] - self.accessibility_times[0]
+                ):
                     self.board.make_accessible()
                     self.accessibility_times.pop(0)
                     self.update_extra_info(("Walls Down"))
 
-            if self.gs.value == GSE.START_SELECTION.value and self.countdown_timer > 3 and self.all_player_starts_selected:
+            if (
+                self.gs.value == GSE.START_SELECTION.value
+                and self.countdown_timer > 3
+                and self.all_player_starts_selected
+            ):
                 self.times[self.current_section] = 3
 
             if self.countdown_timer <= 0:
@@ -161,7 +197,7 @@ class ServerGame(JsonableTick):
                         player.ps.eliminate()
                         self.update_extra_info(("Aborted"))
                     return
-                
+
                 if self.gs.value < GSE.END_GAME.value:
                     print("updating section")
                     self.current_section += 1
@@ -172,7 +208,7 @@ class ServerGame(JsonableTick):
                         self.end_game_events()
                 else:
                     self.determine_ranks_from_capitalize_or_timeout()
-                
+
                 self.gs.next()
 
     def tick(self):
@@ -193,11 +229,12 @@ class ServerGame(JsonableTick):
         if self.board.victory_check() or self.only_bots_remain():
             self.determine_ranks_from_capitalize_or_timeout()
         elif len(self.remaining) == 1:
-
             self.determine_ranks_from_elimination(self.remaining.pop())
 
     def only_bots_remain(self):
-        return all([isinstance(self.player_dict[player], AI) for player in self.remaining])
+        return all(
+            [isinstance(self.player_dict[player], AI) for player in self.remaining]
+        )
 
     def post_tick(self):
         self.extra_info.clear()
@@ -215,10 +252,11 @@ class ServerGame(JsonableTick):
                 if player.count == 0:
                     self.eliminate(player.id, True)
                     if player.killer:
-                        self.update_extra_info(("player_elimination", (player.id, player.killer.id)))
+                        self.update_extra_info(
+                            ("player_elimination", (player.id, player.killer.id))
+                        )
                     else:
                         self.update_extra_info(("timed_out", (player.id)))
-
 
                     print("the killer is", player.killer)
 
@@ -232,7 +270,10 @@ class ServerGame(JsonableTick):
         # score equals 100b + a
         # effectively, the player with the most full capitals wins, with total nodes as a tiebreaker
         # but if already eliminated, they are effectively ranked backwards by multiplying their predetermined rank by -1000
-        player_scores = {key: self.player_dict[key].rank * -1000 + val * 100 + player_nodes[key] for key, val in player_capitals.items()}
+        player_scores = {
+            key: self.player_dict[key].rank * -1000 + val * 100 + player_nodes[key]
+            for key, val in player_capitals.items()
+        }
         sorted_scores = sorted(player_scores.items(), key=lambda x: x[1], reverse=True)
         self.player_dict[sorted_scores[0][0]].win()
         for i in range(1, len(sorted_scores)):
@@ -243,11 +284,17 @@ class ServerGame(JsonableTick):
     def hundred_per_capital(self):
         # total owned full capitals: b
         # maximum of 3
-        return {player.id: self.board.full_player_capitals[player.id] for player in self.player_dict.values()}
+        return {
+            player.id: self.board.full_player_capitals[player.id]
+            for player in self.player_dict.values()
+        }
 
     def only_winner_capitals_count(self):
         # 1 if having 3 full capitals, 0 otherwise
-        return {player.id: int(self.board.full_player_capitals[player.id] >= 3) for player in self.player_dict.values()}
+        return {
+            player.id: int(self.board.full_player_capitals[player.id] >= 3)
+            for player in self.player_dict.values()
+        }
 
     def determine_ranks_from_elimination(self, winner):
         self.player_dict[winner].win()
@@ -257,5 +304,3 @@ class ServerGame(JsonableTick):
                 player.lose()
 
         self.gs.end()
-
-        
