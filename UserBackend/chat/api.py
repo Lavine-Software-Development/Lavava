7ecc4406d539accece35a98cf58c3cc0e4366d92
Gabe Guralnick <gnguralnick@gmail.com>
2024-09-09 14:48:42 -0500

    allow separate chats for each user

--- UserBackend/chat/api.py
+++ UserBackend/chat/api.py
@@ -1,30 +1,42 @@
+import logging
 from flask import Blueprint, jsonify, request
-
+import sys
 
 from .chatbot import Chatbot
+from util import token_required
 
 
 chat_bp = Blueprint('chat', __name__, url_prefix='/chat')
 
-chat_bot = Chatbot()
+user_chats: dict[str, Chatbot] = {}
 
 @chat_bp.route('/')
-def get_chat():
-    return chat_bot.get_chat()
+@token_required
+def get_chat(current_user):
+    if current_user not in user_chats:
+        user_chats[current_user] = Chatbot()
+
+    return user_chats[current_user].get_chat()
 
 @chat_bp.route('/', methods=["POST"])
-def send_chat():
-    # Access the raw text from the request body
+@token_required
+def send_chat(current_user):
+    if current_user not in user_chats:
+        user_chats[current_user] = Chatbot()
+    
     message = request.get_data(as_text=True)
     
     if not message:
-        return jsonify({"error": "No message provided"}), 400  # Return 400 if no body
+        return jsonify({"error": "No message provided"}), 400
     
-    response = chat_bot.send_message(message)
+    response = user_chats[current_user].send_message(message)
     
     return jsonify({'response': response}), 200
 
 @chat_bp.route('/reset', methods=["DELETE"])
-def reset_chat():
-    chat_bot.reset_chat()
+@token_required
+def reset_chat(current_user):
+    if current_user not in user_chats:
+        user_chats[current_user] = Chatbot()
+    user_chats[current_user].reset_chat()
     return jsonify({'message': 'chat successfully reset'}), 200
